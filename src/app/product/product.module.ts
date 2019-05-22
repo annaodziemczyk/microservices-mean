7e8dfe45cbe5b57b73144a81222dedb3ece79f63
annaodziemczyk <anna.odziemczyk@mycit.ie>
2019-05-22 16:41:25 +0100

    fix prod build

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -2,11 +2,13 @@ import {NgModule} from "@angular/core";
 import {AddProductDialog} from "./addproduct/addproduct.component";
 import {ProductService} from "./product.service";
 import {FormsModule} from "@angular/forms";
+import {MatFormFieldModule} from "@angular/material";
 
 
 @NgModule({
   imports: [
-    FormsModule
+    FormsModule,
+    MatFormFieldModule
   ],
   declarations: [
     AddProductDialog
