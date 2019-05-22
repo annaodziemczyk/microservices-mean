a406aaa555e930dfba895dcaa62839e446420ebc
annaodziemczyk <anna.odziemczyk@mycit.ie>
2019-05-22 16:34:37 +0100

    fix prod build

--- src/app/product/product.module.ts
+++ src/app/product/product.module.ts
@@ -1,10 +1,12 @@
 import {NgModule} from "@angular/core";
 import {AddProductDialog} from "./addproduct/addproduct.component";
 import {ProductService} from "./product.service";
+import {FormsModule} from "@angular/forms";
 
 
 @NgModule({
   imports: [
+    FormsModule
   ],
   declarations: [
     AddProductDialog
